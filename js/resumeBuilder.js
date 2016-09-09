6bd54b1bf297e4262b3a6014e82a2fc3bd79531f
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-09-09 20:50:12 +1000

    added the required javascript objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,103 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ /* bio object */
+ var bio = {
+   name: "Shamim Ahmed",
+   role: "Software Developer",
+   contacts: {
+     mobile: "0450289581",
+     email: "shamim.buet.99@gmail.com",
+     github: "shamim-ahmed",
+     twitter: "dreamsmith7",
+     location: "Sydney, Australia"
+   },
+   welcmeMessage: "Welcome",
+   skills: ["Java", "J2EE", "Python", "HTML5", "CSS3"],
+   biopic: "https://avatars0.githubusercontent.com/u/3603688",
+   display: function() {}
+ };
+
+ /* education object */
+ var education = {
+   schools: [
+     {
+       name: "BUET",
+       location: "Dhaka, Bangladesh",
+       degree: "B.Sc in Computer Science and Engineering",
+       majors: ["Computer Science", "Computer Engineering"],
+       dates: "August 2001 - April 2006",
+       url: "http://www.buet.ac.bd"
+     },
+     {
+       name: "Macquarie University",
+       location: "Sydney, Australia",
+       degree: "Master of Information Technology",
+       majors: ["Information Technology", "Web Technologies"],
+       dates: "Jaunary 2012 - April 2013",
+       url: "http://www.mq.edu.au"
+     }
+   ],
+   onlineCourses: [
+     {
+       title: "Android Nanodegree",
+       school: "Udacity",
+       dates: "August 2015 - August 2016",
+       url: "https://www.udacity.com/course/android-developer-nanodegree-by-google--nd801"
+     },
+     {
+       title: "Frontend Developer Nanodegree",
+       school: "Udacity",
+       dates: "August 2016 - Present",
+       url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+     }
+   ],
+   display: function() {}
+ };
+
+ /* work object */
+ var work = {
+   jobs: [
+     {
+       employer: "Escenic Bangladesh",
+       title: "Software Developer",
+       location: "Dhaka, Bangladesh",
+       dates: "January 2007 - December 2011",
+       description: "I worked as a Java/J2EE developer at Escenic Bangladesh."
+     },
+     {
+       employer: "Sandstone Technology",
+       title: "Software Developer",
+       location: "Sydney, Australia",
+       dates: "July 2013 - Present",
+       description: "I am currently working for Sandstone Technology as a Java developer."
+     }
+   ],
+   display: function() {}
+ };
+
+ /* projects object */
+ var projects = {
+   projects: [
+     {
+       title: "",
+       dates: "",
+       description: "",
+       images: []
+     },
+     {
+       title: "",
+       dates: "",
+       description: "",
+       images: []
+     },
+     {
+       title: "",
+       dates: "",
+       description: "",
+       images: []
+     }
+   ],
+   display: function() {}
+ };
