8a3c7c4d97a05996e48abca7501cc3ab5c1f77e8
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-09-10 16:55:32 +1000

    all property names are now quoted

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -21,91 +21,91 @@ This is empty on purpose! Your code to build the resume will go here.
 
  /* education object */
  var education = {
-   schools: [
+   "schools": [
      {
-       name: "BUET",
-       location: "Dhaka, Bangladesh",
-       degree: "B.Sc in Computer Science and Engineering",
-       majors: ["Computer Science", "Computer Engineering"],
-       dates: "August 2001 - April 2006",
-       url: "http://www.buet.ac.bd"
+       "name": "BUET",
+       "location": "Dhaka, Bangladesh",
+       "degree": "B.Sc in Computer Science and Engineering",
+       "majors": ["Computer Science", "Computer Engineering"],
+       "dates": "August 2001 - April 2006",
+       "url": "http://www.buet.ac.bd"
      },
      {
-       name: "Macquarie University",
-       location: "Sydney, Australia",
-       degree: "Master of Information Technology",
-       majors: ["Information Technology", "Web Technologies"],
-       dates: "Jaunary 2012 - April 2013",
-       url: "http://www.mq.edu.au"
+       "name": "Macquarie University",
+       "location": "Sydney, Australia",
+       "degree": "Master of Information Technology",
+       "majors": ["Information Technology", "Web Technologies"],
+       "dates": "Jaunary 2012 - April 2013",
+       "url": "http://www.mq.edu.au"
      }
    ],
-   onlineCourses: [
+   "onlineCourses": [
      {
-       title: "Android Nanodegree",
-       school: "Udacity",
-       dates: "August 2015 - August 2016",
-       url: "https://www.udacity.com/course/android-developer-nanodegree-by-google--nd801"
+       "title": "Android Nanodegree",
+       "school": "Udacity",
+       "dates": "August 2015 - August 2016",
+       "url": "https://www.udacity.com/course/android-developer-nanodegree-by-google--nd801"
      },
      {
-       title: "Frontend Developer Nanodegree",
-       school: "Udacity",
-       dates: "August 2016 - Present",
-       url: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+       "title": "Frontend Developer Nanodegree",
+       "school": "Udacity",
+       "dates": "August 2016 - Present",
+       "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
      }
    ],
-   display: function() {}
+   "display": function() {}
  };
 
  /* work object */
  var work = {
-   jobs: [
+   "jobs": [
      {
-       employer: "Escenic Bangladesh",
-       title: "Software Developer",
-       location: "Dhaka, Bangladesh",
-       dates: "January 2007 - December 2011",
-       description: "I worked as a Java/J2EE developer at Escenic Bangladesh."
+       "employer": "Escenic Bangladesh",
+       "title": "Software Developer",
+       "location": "Dhaka, Bangladesh",
+       "dates": "January 2007 - December 2011",
+       "description": "I worked as a Java/J2EE developer at Escenic Bangladesh."
      },
      {
-       employer: "Sandstone Technology",
-       title: "Software Developer",
-       location: "Sydney, Australia",
-       dates: "July 2013 - Present",
-       description: "I am currently working for Sandstone Technology as a Java developer."
+       "employer": "Sandstone Technology",
+       "title": "Software Developer",
+       "location": "Sydney, Australia",
+       "dates": "July 2013 - Present",
+       "description": "I am currently working for Sandstone Technology as a Java developer."
      }
    ],
-   display: function() {}
+   "display": function() {}
  };
 
  /* projects object */
  var projects = {
-   projects: [
+   "projects": [
      {
-       title: "Escenic Widget Framework",
-       dates: "June 2009 - December 2011",
-       description: "",
-       images: []
+       "title": "Escenic Widget Framework",
+       "dates": "June 2009 - December 2011",
+       "description": "",
+       "images": []
      },
      {
-       title: "Escenic Demo Kit",
-       dates: "June 2009 - December 2011",
-       description: "",
-       images: []
+       "title": "Escenic Demo Kit",
+       "dates": "June 2009 - December 2011",
+       "description": "",
+       "images": []
      },
      {
-       title: "Warehouse Money",
-       dates: "January 2015 - October 2015",
-       description: "",
-       images: []
+       "title": "Warehouse Money",
+       "dates": "January 2015 - October 2015",
+       "description": "",
+       "images": []
      },
      {
-       title: "INGD Orange One",
-       dates: "November 2015 - May 2016",
-       description: "",
-       images: []
+       "title": "INGD Orange One",
+       "dates": "November 2015 - May 2016",
+       "description": "",
+       "images": []
      }
    ],
-   display: function() {}
+   "display": function() {}
  };
 
 /* the logic starts here */
